cb702528d2704031cf44b6e93f292f429a200bef
momo <mohamed1.mrabet@epitech.eu>
2023-02-24 13:23:01 +0100

    fix: clean repo

--- testTechnique/server.js
+++ testTechnique/server.js
@@ -35,7 +35,6 @@ try{
   VALUES (?, ?)
   ON DUPLICATE KEY UPDATE access_token = VALUES(access_token), token_expiry = VALUES(token_expiry)
 `;
-console.log(tokens)
 const values = [ tokens.access_token,  new Date(tokens.expiry_date)];
 
 await db.query(sql, values);
@@ -54,26 +53,6 @@ app.get('/drive', async (req, res) => {
     res.json(data)
 })
 
-// const clientIds = {
-//     account1: '116101226349-q41oo9eudktkvmfgbjhcttdj5n571bs7.apps.googleusercontent.com',
-//     account2: '116101226349-p9unc63vjd3u2kq5sidfqsb3ontgsd88.apps.googleusercontent.com'
-// }
-// const clientSecrets = {
-//     account1: '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',
-//     account2: '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',
-// };
-
-// const redirectUris = {
-//     account1: 'http://localhost:8080/auth/account1/callback',
-//     account2: 'http://localhost:8080/auth/account2/callback',
-// };
-
-// // The scopes that you want to request access to for each account
-// const scopes = {
-//     account1: ['https://www.googleapis.com/auth/drive.metadata.readonly'],
-//     account2: ['https://www.googleapis.com/auth/drive.metadata.readonly'],
-// };
-
 const drive = google.drive({
          version: 'v3',
          auth: oauth2Client
@@ -91,59 +70,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-// app.get('/auth/:account', (req, res) => {
-//     const account = req.params.account;
-//       // Set up the OAuth2.0 client for this account
-//     const oAuthCli = new google.auth.OAuth2(
-//     clientIds[account],
-//     clientSecrets[account],
-//     redirectUris[account]
-//   );
-//   // Generate the authorization URL
-//   const authUrl = oAuthCli.generateAuthUrl({
-//     access_type: 'offline',
-//     scope: scopes[account],
-//   });
-//   // Store the OAuth2.0 client for this account
-//   oAuth2Clients[account] = oAuthCli;
-//   // Redirect the user to the authorization URL
-//   res.redirect(authUrl);
-// });
-
-// let drive = '';
-// app.get('/auth/:account/callback', async (req, res) => {
-//     const account = req.params.account;
-//     const code = req.query.code;
-
-//     if (!code) {
-//         return res.status(400).send('Missing authorization code')
-//       }
-
-//   try {
-//     const { tokens } = await oAuth2Clients[account].getToken(code)
-//     oAuth2Clients[account].setCredentials(tokens)
-//      drive = google.drive({
-//       version: 'v3',
-//       auth: oAuth2Clients[account]
-//     })
-//     res.redirect('/drive')
-//   } catch (err) {
-//     console.error(err)
-//     res.status(500).send('Error exchanging authorization code for access token')
-//   }
-
-// });
-
-  //  app.get('/drive', async (req, res) => {
-  //    const { data } = await drive.files.list({
-  //      fields: 'files(id, name)'
-  //    })
-  //    res.json(data)
-  //  })
-
-
-
-  require("./app/routes/user.routes.js")(app);
+require("./app/routes/user.routes.js")(app);
   // set port, listen for requests
   const PORT = process.env.PORT || 8080;
   app.listen(PORT, () => {
